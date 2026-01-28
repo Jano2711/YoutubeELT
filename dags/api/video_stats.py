4b3b62ada6b48f56e1e517a889469da0f3621eee
Alejandro Negron <alejandroisai42@gmail.com>
2026-01-27 20:01:07 -0600

    Create DAGs functions

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,16 +2,19 @@ import requests
 import json
 from datetime import date
 
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path="./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
-CHANNEL_HANDLE = "MrBeast"
+
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 MAX_RESULTS = 50
 
-
+@task
 def get_playlist_id():
     
     try:
@@ -33,9 +36,8 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
-
        
-
+@task
 def get_video_ids(playListId):
 
     video_ids = []
@@ -62,9 +64,7 @@ def get_video_ids(playListId):
         raise e
 
 
-
-
-
+@task
 def extract_video_data(video_id):
     extracted_data = []
 
@@ -105,6 +105,7 @@ def extract_video_data(video_id):
     except requests.exceptions.RequestException as e:
         raise e
 
+@task
 def save_to_json(extracted_data):
     file_path = f"./data/YT_data_{date.today()}.json"
     with open(file_path, "w", encoding="utf-8") as json_file:
