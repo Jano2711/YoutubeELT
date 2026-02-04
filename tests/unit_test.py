739603bbe15a85558dad59b5e09aeef9f97846dd
Alejandro Negron <alejandroisai42@gmail.com>
2026-02-04 14:09:38 -0600

    Adding unit testing

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,3 +1,48 @@
 def test_api_key(api_key):
     """Test to verify the API key fixture."""
     assert api_key == 'MOCK_KEY1234'
+
+
+def test_channel_handle(channel_handle):
+    assert channel_handle == 'MRCHEESE'
+
+
+def test_postgres_conn(mock_postgres_conn_vars):
+    conn = mock_postgres_conn_vars
+    assert conn.login == 'mock_user'
+    assert conn.password == 'mock_password'
+    assert conn.host == 'mock_host'
+    assert conn.port == 1234
+    assert conn.schema == 'mock_db_name'
+
+def test_dags_integrity(dagbag):
+
+    assert dagbag.import_errors == {}, f"Import errors found: {dagbag.import_errors}"
+    print("===================================")
+    print(dagbag.import_errors)
+
+    expected_dags_ids = ["produce_json","update_db", "data_quality"]
+    loaded_dags_ids = list(dagbag.dags.keys())
+    print("===================================")
+    print(dagbag.dags.keys())
+
+    for dag_id in expected_dags_ids:
+        assert dag_id in loaded_dags_ids, f"DAG '{dag_id}' is missing in the DagBag."
+
+    assert dagbag.size() == 3
+    print("===================================")
+    print(dagbag.size())
+
+    expected_tasks_count = {
+        "produce_json": 4,
+        "update_db": 2,
+        "data_quality": 2
+    }
+    print("===================================")
+    for dag_id, dag in dagbag.dags.items():
+        actual_task_count = len(dag.tasks)
+        expected_count = expected_tasks_count[dag_id]
+        assert (
+            expected_count == actual_task_count
+            ), f"DAG '{dag_id}' has {actual_task_count} tasks; expected {expected_count}."
+        print(dag_id, len(dag.tasks))
