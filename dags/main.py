6193d6cc44a49f5419ef209c288a14af2b98565d
Alejandro Negron <alejandroisai42@gmail.com>
2026-02-03 21:01:52 -0600

    Adding dags to main function

--- dags/main.py
+++ dags/main.py
@@ -3,8 +3,11 @@ import pendulum
 from datetime import timedelta, datetime
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 
+from datawarehouse.dwh import stagin_table, core_table
+from dataquality.soda import yt_elt_data_quality
+
 # Define the local timezone
-local_tz = pendulum.timezone("America/Bahia_Banderas")
+local_tz = pendulum.timezone("America/Mexico_City")
 
 # Default Args
 default_args = {
@@ -16,10 +19,14 @@ default_args = {
     #"retries": 1,
     #"retry_delay": timedelta(minutes=5),
     "max_active_runs": 1,
-    "dagrun_timeout": timedelta(minutes=60),
+    "dagrun_timeout": timedelta(hours=1),
     "start_date": datetime(2026, 1, 1, tzinfo=local_tz),
     #"end_date": datetime(2026, 12, 31, tzinfo=local_tz),
 }
+
+# Variable
+stagin_schema = 'staging'
+core_schema = 'core'
 
 with DAG(
     dag_id = 'produce_json',
@@ -35,3 +42,31 @@ with DAG(
 
     # Set task dependencies
     playlist_id >> video_ids >> extract_data >> save_to_json_task
+
+with DAG(
+    dag_id = 'update_db',
+    default_args=default_args,
+    description='DAG to process JSON data into Data Warehouse',
+    schedule = '0 15 * * *',
+    catchup = False
+) as dag:
+    
+    update_staging = stagin_table()
+    update_core = core_table()
+
+    # Set task dependencies
+    update_staging >> update_core 
+
+with DAG(
+    dag_id = 'data_quality',
+    default_args=default_args,
+    description='DAG to check data quality on both layers in the db using Soda',
+    schedule = '0 16 * * *',
+    catchup = False
+) as dag:
+    
+    soda_validate_staging = yt_elt_data_quality(stagin_schema)
+    soda_validate_core = yt_elt_data_quality(core_schema)
+
+    # Set task dependencies
+    soda_validate_staging >> soda_validate_core 
