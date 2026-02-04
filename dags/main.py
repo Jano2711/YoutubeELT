4a3447fa8cdd963e55de7715c5f5d4fab41208ff
Alejandro Negron <alejandroisai42@gmail.com>
2026-02-04 15:32:25 -0600

    Adding dag dependencies

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 import pendulum
 from datetime import timedelta, datetime
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
 
 from datawarehouse.dwh import stagin_table, core_table
@@ -34,36 +36,45 @@ with DAG(
     description='DAG to produce JSON file with raw data',
     schedule = '0 14 * * *',
     catchup = False
-) as dag:
+) as dag_produce:
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id='trigger_update_db',
+        trigger_dag_id='update_db',
+        )
+
+
     # Set task dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 with DAG(
     dag_id = 'update_db',
     default_args=default_args,
     description='DAG to process JSON data into Data Warehouse',
-    schedule = '0 15 * * *',
     catchup = False
-) as dag:
+) as dag_update:
     
     update_staging = stagin_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id='trigger_data_quality',
+        trigger_dag_id='data_quality',
+    )
+
     # Set task dependencies
-    update_staging >> update_core 
+    update_staging >> update_core >> trigger_data_quality
 
 with DAG(
     dag_id = 'data_quality',
     default_args=default_args,
     description='DAG to check data quality on both layers in the db using Soda',
-    schedule = '0 16 * * *',
     catchup = False
-) as dag:
+) as dag_quality:
     
     soda_validate_staging = yt_elt_data_quality(stagin_schema)
     soda_validate_core = yt_elt_data_quality(core_schema)
